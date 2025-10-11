eb27e901f6899328a09f6010d1b3ef6128273631
Javier Sánchez <78811548+JavierSanchez17@users.noreply.github.com>
2025-10-11 00:24:11 -0600

    feature: add indentify line on files

--- Assets/Scripts/MVC/Controllers/EnemyController.cs
+++ Assets/Scripts/MVC/Controllers/EnemyController.cs
@@ -1,10 +1,1 @@
-public class EnemyController : MonoBehaviour
-{
-    [SerializeField] private EnemyFactoryProvider factory;
-
-    public void SpawnEnemy(Vector3 pos)
-    {
-        var enemy = factory.CreateEnemy(pos, Quaternion.identity);
-        // Iniciar enemy
-    }
-}
+using UnityEngine;
--- Assets/Scripts/Patterns/Factory/EnemyFactroryProvider.cs
+++ Assets/Scripts/Patterns/Factory/EnemyFactroryProvider.cs
@@ -1,12 +1,1 @@
 using UnityEngine;
-
-public class EnemyFactoryProvider : MonoBehaviour, IEnemyFactory
-{
-    [SerializeField] private GameObject enemyPrefab;
-
-    public EnemyModel CreateEnemy(Vector3 position, Quaternion rotation)
-    {
-        var obj = Instantiate(enemyPrefab, position, rotation);
-        return obj.GetComponent<EnemyModel>();
-    }
-}
