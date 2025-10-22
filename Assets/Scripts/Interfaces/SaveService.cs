0b1708dc6c62077b14c9aab2c956c8ebb9918327
Javier Sánchez <78811548+JavierSanchez17@users.noreply.github.com>
2025-10-22 00:45:07 -0600

    chore: name to interface

--- Assets/Scripts/Interfaces/SaveService.cs
+++ Assets/Scripts/Interfaces/SaveService.cs
@@ -1,7 +1,7 @@
-namespace RedRunner.Interfaces
+namespace DinoRunner.Interfaces
 {
     // Interface for saving and loading game data
-    public interface ISaveService
+    public interface SaveService
     {
         void SaveGame<T>(string key, T data);
         T LoadGame<T>(string key, T defaultValue = default); // Use default if key not found
