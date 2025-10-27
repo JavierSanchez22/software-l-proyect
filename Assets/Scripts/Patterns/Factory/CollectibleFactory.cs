daae85f464386a60429fc6db51d1628ca9b1a28e
Allan Eduardo Perez Ajanel <allanperezajanel@gmail.com>
2025-10-27 16:32:47 -0600

    fix(factory): fix a duplicate function on CollectibleFactory

--- Assets/Scripts/Patterns/Factory/CollectibleFactory.cs
+++ Assets/Scripts/Patterns/Factory/CollectibleFactory.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+using DinoRunner.Patterns.Singleton;
+
 namespace DinoRunner.Patterns.Factory
 {
-    public class CollectibleFactory :
-    Singleton.SingletonMonoBehaviour<CollectibleFactory>
+    public class CollectibleFactory : 
+        SingletonMonoBehaviour<CollectibleFactory>
     {
         [System.Serializable]
         public class CollectiblePrefabMapping
         {
-            public string collectibleTypeIdentifier; 
+            public string collectibleTypeIdentifier;
             public GameObject collectiblePrefab;
         }
+
         [Header("Collectible Prefabs")]
         [SerializeField] private List<CollectiblePrefabMapping> collectiblePrefabs;
+        
         [SerializeField] private Transform collectibleContainer;
-        // TODO: Implement Object Pooling
+
         protected override void Awake()
         {
             base.Awake();
@@ -24,38 +28,21 @@ namespace DinoRunner.Patterns.Factory
                 collectibleContainer = new GameObject("---Collectibles---").transform;
             }
         }
+
         public GameObject CreateCollectible(string typeIdentifier, Vector3 position)
         {
             CollectiblePrefabMapping mapping = collectiblePrefabs.Find(m =>
             m.collectibleTypeIdentifier == typeIdentifier);
+
             if (mapping == null || mapping.collectiblePrefab == null)
             {
                 Debug.LogError($"[CollectibleFactory] Prefab not found for type: {typeIdentifier} ");
                 return null;
             }
-            // TODO: Replace Instantiate with pool logic
+
             GameObject instance = Instantiate(mapping.collectiblePrefab,
             position, Quaternion.identity, collectibleContainer);
-            instance.name = $"{typeIdentifier}_{Time.time}";
-            return instance;
-        }
-    }
-    
 
-
-public GameObject CreateCollectible(string typeIdentifier, Vector3 position)
-        {
-            CollectiblePrefabMapping mapping = collectiblePrefabs.Find(m =>
-            m.collectibleTypeIdentifier == typeIdentifier);
-            if (mapping == null || mapping.collectiblePrefab == null)
-            {
-                Debug.LogError($"[CollectibleFactory] Prefab not found for type: { typeIdentifier} ");
-                return null;
-            }
-
-            // TODO: Replace Instantiate with pool logic
-            GameObject instance = Instantiate(mapping.collectiblePrefab,
-            position, Quaternion.identity, collectibleContainer);
             instance.name = $"{typeIdentifier}_{Time.time}";
             return instance;
         }
